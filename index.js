ffe89686a4b3a4fcb260716eda6d65d44525e6bd
eunbin <dmsqls1007@naver.com>
2023-03-20 17:26:46 +0900

    ♻️ cors : origin 옵션 추가(실서버만)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const app = express();
 const port = 8080;
 
-app.use(cors());
+app.use(cors({ origin: "https://https://nearbybooks.vercel.app/" }));
 
 app.get('/', (req, res) => {
   orderFetch(res);
