4c3e863973e4570d2a0794857ed70793470bbdc9
eunbin <dmsqls1007@naver.com>
2023-03-20 17:34:27 +0900

    ✏️ origin 오타 수정

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch')
 const app = express();
 const port = 8080;
 
-app.use(cors({ origin: "https://https://nearbybooks.vercel.app/" }));
+app.use(cors({ origin: "https://nearbybooks.vercel.app" }));
 
 app.get('/', (req, res) => {
   orderFetch(res);
